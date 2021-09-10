6f604d34d5cbf2799fddbfb9dda7ec31513a3cab
Ayusha Kar <B520015@iiit-bh.ac.in>
2021-09-10 22:29:33 +0530

    Script correction

--- port.js
+++ port.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
@@ -8,15 +7,4 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
     })
-=======
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }
-        else{
-            $('.navbar').removeClass("sticky");
-        }
-    })
->>>>>>> 463f96f83e3e3aeea10c2d65ec520378082dea3a
-});
+});    
